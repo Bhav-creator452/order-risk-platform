ebbcb5dd23ba413db413f7e0ff83e0bf5e512054
Bhav-creator452 <bhavyakaur10@gmail.com>
2026-06-29 18:10:28 +0530

    import fix

--- src/rules.py
+++ src/rules.py
@@ -1,6 +1,6 @@
 import logging
 from dataclasses import dataclass
-from .models import Order 
+from models import Order 
 from abc import ABC , abstractmethod
 logging.basicConfig(level=logging.WARNING
                    ,format="%(asctime)s - %(levelname)s - %(message)s")
